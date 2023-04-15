a5615b1b2c7c1c34195faaa43eece71901b62637
TimothyHermawan <timothyhermawan@gmail.com>
2023-04-15 13:27:07 +0700

    Added Singleton ScriptableObject with Addressable or Resource

---
+++ SingletonScriptableObject.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+#if UNITY_ADDRESSABLE
+using UnityEngine.AddressableAssets;
+#endif
+
+public class SingletonScriptableObject<T> : ScriptableObject where T : ScriptableObject
+{
+    private static T _instance;
+
+
+#if UNITY_ADDRESSABLE
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = Addressables.LoadAssetAsync<T>(typeof(T).Name).Result;
+            }
+            return _instance;
+        }
+    }
+
+#else
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = Resources.LoadAsync<T>(typeof(T).Name).asset as T;
+            }
+            return _instance;
+        }
+    }
+
+#endif
+}
